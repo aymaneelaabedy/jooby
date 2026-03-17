ae9caeab806f7e15aeef584cdba3c2f4d02bc652
aymaneelaabedy <aymane.elabidi@gmail.com>
2026-03-17 22:29:21 +0000

    first push for test

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Jooby",
+  description: "Job platform"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
